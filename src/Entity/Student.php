efa734095fc0b041d0a21e3b0787f72484ec55be
Albert Arques <albertarques@gmail.com>
2023-10-19 19:10:21 +0200

    Created Student Entity

---
+++ src/Entity/Student.php
@@ -0,0 +1,261 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StudentRepository::class)]
+class Student
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $address = null;
+
+    #[ORM\Column]
+    private ?int $payment = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $schedule = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $comments = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $surname = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $birth_date = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $city_id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $parent_id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $bank_id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $dni = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $phone = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $tariff_id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $has_image_rights_accepted = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $has_sepa_agreement_accepted = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): static
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getPayment(): ?int
+    {
+        return $this->payment;
+    }
+
+    public function setPayment(int $payment): static
+    {
+        $this->payment = $payment;
+
+        return $this;
+    }
+
+    public function getSchedule(): ?string
+    {
+        return $this->schedule;
+    }
+
+    public function setSchedule(?string $schedule): static
+    {
+        $this->schedule = $schedule;
+
+        return $this;
+    }
+
+    public function getComments(): ?string
+    {
+        return $this->comments;
+    }
+
+    public function setComments(?string $comments): static
+    {
+        $this->comments = $comments;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): static
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getBirthDate(): ?\DateTimeInterface
+    {
+        return $this->birth_date;
+    }
+
+    public function setBirthDate(\DateTimeInterface $birth_date): static
+    {
+        $this->birth_date = $birth_date;
+
+        return $this;
+    }
+
+    public function getCityId(): ?int
+    {
+        return $this->city_id;
+    }
+
+    public function setCityId(?int $city_id): static
+    {
+        $this->city_id = $city_id;
+
+        return $this;
+    }
+
+    public function getParentId(): ?int
+    {
+        return $this->parent_id;
+    }
+
+    public function setParentId(?int $parent_id): static
+    {
+        $this->parent_id = $parent_id;
+
+        return $this;
+    }
+
+    public function getBankId(): ?int
+    {
+        return $this->bank_id;
+    }
+
+    public function setBankId(?int $bank_id): static
+    {
+        $this->bank_id = $bank_id;
+
+        return $this;
+    }
+
+    public function getDni(): ?string
+    {
+        return $this->dni;
+    }
+
+    public function setDni(?string $dni): static
+    {
+        $this->dni = $dni;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): static
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getTariffId(): ?int
+    {
+        return $this->tariff_id;
+    }
+
+    public function setTariffId(?int $tariff_id): static
+    {
+        $this->tariff_id = $tariff_id;
+
+        return $this;
+    }
+
+    public function isHasImageRightsAccepted(): ?bool
+    {
+        return $this->has_image_rights_accepted;
+    }
+
+    public function setHasImageRightsAccepted(?bool $has_image_rights_accepted): static
+    {
+        $this->has_image_rights_accepted = $has_image_rights_accepted;
+
+        return $this;
+    }
+
+    public function isHasSepaAgreementAccepted(): ?bool
+    {
+        return $this->has_sepa_agreement_accepted;
+    }
+
+    public function setHasSepaAgreementAccepted(?bool $has_sepa_agreement_accepted): static
+    {
+        $this->has_sepa_agreement_accepted = $has_sepa_agreement_accepted;
+
+        return $this;
+    }
+}

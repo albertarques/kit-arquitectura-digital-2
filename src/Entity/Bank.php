94117337b2b0110a7a8592902747a949d257ab2e
Albert Arques <albertarques@gmail.com>
2023-10-19 19:39:41 +0200

    Added Bank entity

---
+++ src/Entity/Bank.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BankRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BankRepository::class)]
+class Bank
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $account_number = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $swift_code = null;
+
+    #[ORM\OneToMany(mappedBy: 'bank_id', targetEntity: Student::class)]
+    private Collection $students;
+
+    public function __construct()
+    {
+        $this->students = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAccountNumber(): ?string
+    {
+        return $this->account_number;
+    }
+
+    public function setAccountNumber(?string $account_number): static
+    {
+        $this->account_number = $account_number;
+
+        return $this;
+    }
+
+    public function getSwiftCode(): ?string
+    {
+        return $this->swift_code;
+    }
+
+    public function setSwiftCode(?string $swift_code): static
+    {
+        $this->swift_code = $swift_code;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Student>
+     */
+    public function getStudents(): Collection
+    {
+        return $this->students;
+    }
+
+    public function addStudent(Student $student): static
+    {
+        if (!$this->students->contains($student)) {
+            $this->students->add($student);
+            $student->setBankId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeStudent(Student $student): static
+    {
+        if ($this->students->removeElement($student)) {
+            // set the owning side to null (unless already changed)
+            if ($student->getBankId() === $this) {
+                $student->setBankId(null);
+            }
+        }
+
+        return $this;
+    }
+}

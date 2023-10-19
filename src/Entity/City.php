15dcd08e57fc531e38300dde8e8a9821970ec525
Albert Arques <albertarques@gmail.com>
2023-10-19 19:41:28 +0200

    Added City entity

---
+++ src/Entity/City.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CityRepository::class)]
+class City
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $postal_code = null;
+
+    #[ORM\ManyToOne(inversedBy: 'cities')]
+    private ?Province $province_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?string
+    {
+        return $this->postal_code;
+    }
+
+    public function setPostalCode(string $postal_code): static
+    {
+        $this->postal_code = $postal_code;
+
+        return $this;
+    }
+
+    public function getProvinceId(): ?Province
+    {
+        return $this->province_id;
+    }
+
+    public function setProvinceId(?Province $province_id): static
+    {
+        $this->province_id = $province_id;
+
+        return $this;
+    }
+}

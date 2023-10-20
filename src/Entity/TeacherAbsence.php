d517a32d109bb39d6f53d4ebbc02bdeda412ea89
Albert Arques <albertarques@gmail.com>
2023-10-20 07:22:39 +0200

    Added TeacherAbscence entity

---
+++ src/Entity/TeacherAbsence.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TeacherAbsenceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TeacherAbsenceRepository::class)]
+class TeacherAbsence
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'teacherAbsences')]
+    private ?Teacher $teacher_id = null;
+
+    #[ORM\Column]
+    private ?int $type = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $day = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTeacherId(): ?Teacher
+    {
+        return $this->teacher_id;
+    }
+
+    public function setTeacherId(?Teacher $teacher_id): static
+    {
+        $this->teacher_id = $teacher_id;
+
+        return $this;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    public function setType(int $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDay(): ?\DateTimeInterface
+    {
+        return $this->day;
+    }
+
+    public function setDay(\DateTimeInterface $day): static
+    {
+        $this->day = $day;
+
+        return $this;
+    }
+}

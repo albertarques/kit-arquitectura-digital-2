391109931db22ece6a80881f76e4369c18ae5a91
Albert Arques <albertarques@gmail.com>
2023-10-20 07:14:14 +0200

    Added City relationship with Student

--- src/Entity/City.php
+++ src/Entity/City.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CityRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CityRepository::class)]
@@ -21,6 +23,14 @@ class City
 
     #[ORM\ManyToOne(inversedBy: 'cities')]
     private ?Province $province_id = null;
+
+    #[ORM\OneToMany(mappedBy: 'city_id', targetEntity: Student::class)]
+    private Collection $students;
+
+    public function __construct()
+    {
+        $this->students = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -59,6 +69,36 @@ class City
     public function setProvinceId(?Province $province_id): static
     {
         $this->province_id = $province_id;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Student>
+     */
+    public function getStudents(): Collection
+    {
+        return $this->students;
+    }
+
+    public function addStudent(Student $student): static
+    {
+        if (!$this->students->contains($student)) {
+            $this->students->add($student);
+            $student->setCityId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeStudent(Student $student): static
+    {
+        if ($this->students->removeElement($student)) {
+            // set the owning side to null (unless already changed)
+            if ($student->getCityId() === $this) {
+                $student->setCityId(null);
+            }
+        }
 
         return $this;
     }
